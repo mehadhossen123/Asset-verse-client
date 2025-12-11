d8977fd23d6e828397f7e4ec5dd43f6c0f9a29a1
Mehad Hossen <mehad004938@gmail.com>
2025-12-11 08:30:26 +0600

    make asset edit api and make a modal

--- src/Components/Package.jsx
+++ src/Components/Package.jsx
@@ -1,6 +1,9 @@
 import { useQuery } from "@tanstack/react-query";
 import React from "react";
 import useAxiosSecure from "../Hooks/useAxiosSecure";
+import { TiTickOutline } from "react-icons/ti";
+import { motion } from "framer-motion";
+
 
 const Package = () => {
   const useAxios = useAxiosSecure();
@@ -13,85 +16,71 @@ const Package = () => {
   });
 
   return (
-    <div className="grid grid-cols-3">
-     
-      {pakData.map((data, i) => (
-       
-          <div
+    <>
+    <h1 className="text-white text-center text-4xl font-bold">Our All Packages </h1>
+      <div className="grid grid-cols-3">
+        {pakData.map((data, i) => (
+          <motion.div
+            animate={{ y: [0, -30, 0, 30, 0] }}
+            transition={{ duration: 1.5, repeat: Infinity, ease: "easeInOut" }}
             key={i}
-            class="w-full max-w-sm mx-auto  bg-white rounded-xl shadow-2xl border-indigo-600 overflow-hidden transform hover:scale-[1.02] transition duration-300 ease-in-out border-2  
+            class="w-full max-w-sm mx-auto my-5  bg-white rounded-xl shadow-2xl border-indigo-600 overflow-hidden transform hover:scale-[1.02] transition duration-300 ease-in-out border-2  
     "
           >
             <div
-              class="p-4 text-center  bg-indigo-50
+              className="p-4 text-center  bg-indigo-50
         "
             >
-              <h3 class="text-3xl font-bold text-gray-800 tracking-tight">
+              <h3 className="text-3xl font-bold text-gray-800 tracking-tight">
                 {data.name}
               </h3>
 
               {data.name === "Standard" && (
-                <span class="inline-block bg-indigo-600 text-white text-xs font-semibold px-3 py-1 mt-2 rounded-full uppercase">
+                <span className="inline-block bg-indigo-600 text-white text-xs font-semibold px-3 py-1 mt-2 rounded-full uppercase">
                   Most Popular
                 </span>
               )}
             </div>
 
-            <div class="px-4 pb-4 text-center">
-              <p class="text-5xl font-extrabold text-gray-900">
+            <div className="px-4 pb-4 text-center">
+              <p className="text-5xl font-extrabold text-gray-900">
                 ${data.price}
-                <span class="text-xl font-medium text-gray-500">/mo</span>
+                <span className="text-xl font-medium text-gray-500">/mo</span>
               </p>
-              <p class="mt-2 text-sm text-gray-600">
+              <p className="mt-2 text-sm text-gray-600">
                 Up to {data.employeeLimit} Employees
               </p>
             </div>
 
-            <div class="px-4 py-4 bg-gray-50 border-t border-gray-200">
-              <h4 class="text-lg font-semibold text-gray-700 mb-3">
+            <div className="px-4 py-4 bg-gray-50 border-t border-gray-200">
+              <h4 className="text-lg font-semibold text-yellow-700 mb-3">
                 Key Features:
               </h4>
-              <ul class="space-y-3 text-sm text-gray-700">
+              <ul className=" text-sm text-gray-700">
                 {data.features.map((feature, index) => (
-                  <li key={index} class="flex items-start">
+                  <li key={index} className="flex items-start">
+                    
+                    <TiTickOutline className="text-blue-600" />
 
-{/*                     
-                    <span class="text-green-500 mr-3 mt-1">
-                      <svg
-                        class="w-5 h-5"
-                        fill="none"
-                        stroke="currentColor"
-                        viewBox="0 0 24 24"
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth="2"
-                          d="M5 13l4 4L19 7"
-                        ></path>
-                      </svg>
-                    </span> */}
-
-
-                    <span class="flex-1">{feature}</span>
+                    <span className="flex-1">{feature}</span>
                   </li>
                 ))}
               </ul>
             </div>
 
-            <div class="p-4">
+            <div className="p-4">
               <button
-                class="w-full py-3 rounded-lg text-lg font-semibold transition duration-200 
+                className="w-full py-3 rounded-lg text-lg font-semibold transition duration-200 
          
             ${data.name === 'Standard' ? 'bg-indigo-600 text-white hover:bg-indigo-700' : 'bg-gray-200 text-gray-700 hover:bg-gray-300'}"
               >
                 Choose {data.name}
               </button>
             </div>
-          </div>
-      
-      ))}
-    </div>
+          </motion.div>
+        ))}
+      </div>
+    </>
   );
 };
 
--- src/Manager/HrAssetList.jsx
+++ src/Manager/HrAssetList.jsx
@@ -1,26 +1,170 @@
 import { useQuery } from '@tanstack/react-query';
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import useAxiosSecure from '../Hooks/useAxiosSecure';
 import useAuth from '../Hooks/useAuth';
 import { FaRegEdit } from 'react-icons/fa';
 import { MdDeleteOutline } from 'react-icons/md';
 import { motion } from 'framer-motion';
-
+import Swal from 'sweetalert2';
+import { useForm } from 'react-hook-form';
+import useAxios from '../Hooks/useAxios';
 
 
 const HrAssetList = () => {
-    const axiosSecure=useAxiosSecure()
-    const {user}=useAuth()
-    const {data:hrAssets=[]}=useQuery({
-        queryKey:["assets",user?.email],
-        queryFn:async ()=>{
-            const res=await axiosSecure.get(`/assets?email=${user?.email}`)
-            return res.data.data;
-        }
-    })
+ const { reset, register, handleSubmit } = useForm();
+ const [assetId, setAssetId] = useState();
+ const axios=useAxios()
 
-     
-    return (
+
+
+  const modalRef=useRef()
+  
+  const { user } = useAuth();
+  const axiosSecure=useAxiosSecure()
+  const { data: hrAssets = [] ,refetch} = useQuery({
+    queryKey: ["assets", user?.email],
+    queryFn: async () => {
+      const res = await axiosSecure.get(`/assets?email=${user?.email}`);
+      return res.data.data;
+    },
+  });
+
+  //handleDeleteAsset
+  const handleDeleteAsset=(id)=>{
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+
+        axiosSecure.delete(`/assets/${id}`).then((res) => {
+          console.log(res.data)
+          refetch()
+           Swal.fire({
+             title: "Deleted!",
+             text:`${res?.data?.message}`,
+             icon: "success",
+           });
+         
+        });
+       
+      }
+    });
+  }
+
+  const handleOpenModal=(id)=>{
+    setAssetId(id)
+    modalRef.current.showModal()
+
+  }
+  const  handleEditProduct=async(data)=>{
+    console.log(assetId)
+    console.log(data)
+     const productImage = data.productImage[0];
+    const formData = new FormData();
+    formData.append("image", productImage);
+
+    const image_api_url =`https://api.imgbb.com/1/upload?key=${import.meta.env.VITE_IMG_API_KEY }`;
+
+    const product_image_url = await axios .post(image_api_url,formData);
+    const productImageUrl= product_image_url.data.data.display_url;
+    console.log(productImageUrl)
+  
+   const updatedInfo = {
+     productName: data.productName,
+     productImage: productImageUrl,
+     productType: data.productType,
+     productQuantity: parseInt(data.productQuantity),
+   };
+   axiosSecure.patch(`/assets/${assetId}`,updatedInfo).then((res)=>{
+    reset()
+    refetch()
+    console.log(res.data)
+   })
+
+   
+  }
+
+  return (
+    <>
+      {/* Edit modal si here  */}
+      {/* Open the modal using document.getElementById('ID').showModal() method */}
+
+      <dialog ref={modalRef} className="modal modal-bottom sm:modal-middle">
+        {/* Modal body is here  */}
+        <div className="modal-box">
+          <h3 className="font-bold text-yellow-700 text-center text-3xl">
+            Edit Your Asset
+          </h3>
+          {/* Edit part  */}
+          <form
+            onSubmit={handleSubmit(handleEditProduct)}
+            className="space-y-3"
+          >
+            {/* Product Name */}
+            <div>
+              <label className="text-sm font-medium">Product Name</label>
+              <input
+                {...register("productName")}
+                type="text"
+                className="w-full p-2 border rounded-md mt-1 text-sm"
+                placeholder="Enter product name"
+              />
+            </div>
+
+            {/* Product Image */}
+            <div>
+              <label className="text-sm font-medium mb-1">Product Image</label>
+              <input
+                {...register("productImage")}
+                type="file"
+                className="file-input file-input-primary w-full"
+              />
+            </div>
+
+            {/* Product Type */}
+            <div>
+              <label className="text-sm font-medium">Product Type</label>
+              <select
+                {...register("productType")}
+                name="productType"
+                className="w-full p-2 border rounded-md mt-1 text-sm"
+              >
+                <option value="">Select Type</option>
+                <option value="Returnable">Returnable</option>
+                <option value="Non-returnable">Non-returnable</option>
+              </select>
+            </div>
+
+            {/* Product Quantity */}
+            <div>
+              <label className="text-sm font-medium">Product Quantity</label>
+              <input
+                {...register("productQuantity")}
+                type="number"
+                className="w-full p-2 border rounded-md mt-1 text-sm"
+                placeholder="Enter quantity"
+              />
+            </div>
+
+            <button type="submit" className="button w-full">
+              Update
+            </button>
+          </form>
+
+          <div className="modal-action">
+            <form method="dialog">
+              {/* if there is a button in form, it will close the modal */}
+              <button className="btn">Close</button>
+            </form>
+          </div>
+        </div>
+      </dialog>
       <div>
         {hrAssets.length === 0 ? (
           <div>
@@ -111,10 +255,16 @@ const HrAssetList = () => {
                     {new Date(asset.dateAdded).toLocaleDateString()}
                   </td>
                   <td className="text-center text-white">
-                    <button className="px-6 py-1 text-2xl cursor-pointer mr-4 bg-gradient-to-r from-blue-500 to-cyan-400 hover:from-cyan-400 hover:to-blue-600 text-white font-semibold rounded-lg shadow-md hover:bg-indigo-700 transition duration-300">
+                    <button
+                      onClick={() => handleOpenModal(asset._id)}
+                      className="px-6 py-1 text-2xl cursor-pointer mr-4 bg-gradient-to-r from-blue-500 to-cyan-400 hover:from-cyan-400 hover:to-blue-600 text-white font-semibold rounded-lg shadow-md hover:bg-indigo-700 transition duration-300"
+                    >
                       <FaRegEdit />
                     </button>
-                    <button className="px-6 py-1 cursor-pointer text-2xl bg-gradient-to-r from-red-500 to-rose-400 hover:from-rose-400 hover:to-red-600 text-white font-semibold rounded-lg shadow-md transition duration-300 ">
+                    <button
+                      onClick={() => handleDeleteAsset(asset._id)}
+                      className="px-6 py-1 cursor-pointer text-2xl bg-gradient-to-r from-red-500 to-rose-400 hover:from-rose-400 hover:to-red-600 text-white font-semibold rounded-lg shadow-md transition duration-300 "
+                    >
                       <MdDeleteOutline />
                     </button>
                   </td>
@@ -124,7 +274,8 @@ const HrAssetList = () => {
           </table>
         </div>
       </div>
-    );
+    </>
+  );
 };
 
 export default HrAssetList;
