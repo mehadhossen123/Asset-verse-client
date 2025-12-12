16e5d89ffd21369c9c1896c420fdc5a38c6dcd2d
Mehad Hossen <mehad004938@gmail.com>
2025-12-12 19:29:33 +0600

    make pagination

--- src/Employee/RequestAsset.jsx
+++ src/Employee/RequestAsset.jsx
@@ -1,48 +1,75 @@
-import React from "react";
-import useAxiosSecure from "../Hooks/useAxiosSecure";
-import { useQuery } from "@tanstack/react-query";
+import React, { useEffect, useState } from "react";
+
 import { useNavigate } from "react-router";
 import { motion } from "framer-motion";
-import Loader from "../Components/Loading/Loader";
+
+import { MdOutlineWebAsset } from "react-icons/md";
 
 const RequestAsset = () => {
-  const axiosSecure = useAxiosSecure();
-  const navigate=useNavigate()
+  const [searchText, setSearchText] = useState("");
+  const [requestAsset, setRequestAsset] = useState([]);
+  const navigate = useNavigate();
+  const [page, setPage] = useState(0);
+  const [totalApps, setTotalApps] = useState(0);
+  const [currentPage, setCurrentPage] = useState(0);
+  // console.log(page, totalApps);
+  console.log(searchText)
 
-  const { data: requestAsset = [] ,isLoading} = useQuery({
-    queryKey: ["assets"],
-    queryFn: async () => {
-      const res = await axiosSecure.get(`/assets`);
-      return res.data.data;
-    },
-  });
+  const limit = 8;
+
+  useEffect(() => {
+    fetch(
+      ` http://localhost:5000/assets?limit=${limit}&skip=${
+        currentPage * limit
+      }&search=${searchText}`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setRequestAsset(data.data);
+        setTotalApps(data.total);
+        setPage(Math.ceil(data.total / limit));
+      });
+  }, [currentPage,searchText]);
 
   const handleViewDetails = (id) => {
     navigate(`/dashboard/${id}`);
   };
-  if(isLoading){
-    return<Loader></Loader>
-  }
-
 
   return (
     <div className="p-6  min-h-screen">
-      <h1 className="text-3xl font-bold mb-6 text-gray-800">
-        Request Assets ({requestAsset.length})
+      <h1 className="text-3xl font-bold mb-6  text-white text-center">
+        Request Assets ({totalApps})
       </h1>
+      {/* Search bar  */}
+      <div className="flex justify-center my-5 ">
+        <div className="join">
+          <div>
+            <label className="input validator join-item w-[700px]">
+              <MdOutlineWebAsset className="text-2xl" />
+              <input
+                onChange={(e) => setSearchText(e.target.value)}
+                type="text"
+                placeholder="Search By asset name"
+                required
+              />
+            </label>
+          </div>
+          <button className=" bg-amber-600 join-item cursor-pointer  font-bold py-1 px-1">
+            Search
+          </button>
+        </div>
+      </div>
 
       <div className="grid lg:grid-cols-4 md:grid-cols-2 sm:grid-cols-1 gap-6">
         {requestAsset.map((asset) => (
           <motion.div
-            style={{
-            }}
-            whileHover={{ scale:1.05 }}
+            style={{}}
+            whileHover={{ scale: 1.05 }}
             key={asset._id}
             className="bg-white rounded-lg shadow-md overflow-hidden hover:shadow-xl transition-shadow duration-300 flex flex-col"
           >
             {/* Asset Image */}
             <img
-             
               src={asset.productImage}
               alt={asset.productName}
               className="w-full h-48 object-cover hover:scale-110 hover:duration-1000"
@@ -71,6 +98,35 @@ const RequestAsset = () => {
             </div>
           </motion.div>
         ))}
+      </div>
+      <div className="flex justify-center flex-wrap  py-10 gap-5">
+        {currentPage > 0 && (
+          <button
+            onClick={() => setCurrentPage(currentPage - 1)}
+            className="btn"
+          >
+            Prev
+          </button>
+        )}
+        {[...Array(page).keys()].map((i) => (
+          <button
+            key={i}
+            onClick={() => setCurrentPage(i)}
+            className={` btn text-black font-bold border-0 ${
+              i === currentPage && "btn-primary"
+            }`}
+          >
+            {i + 1}
+          </button>
+        ))}
+        {currentPage < page - 1 && (
+          <button
+            onClick={() => setCurrentPage(currentPage + 1)}
+            className="btn"
+          >
+            Next
+          </button>
+        )}
       </div>
     </div>
   );
